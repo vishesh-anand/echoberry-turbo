93ff0e6da2533a909a387d2512e00863558a95b3
Vishesh Anand <visheshanand3@gmail.com>
2025-07-06 13:11:09 +0530

    setup shared package

--- packages/shared/types.ts
+++ packages/shared/types.ts
@@ -1,6 +1,20 @@
 import { z } from "zod";
 
 // childName, childAge, artStyle, values, theme, customTheme, photoUrls
-const GenerateStoryBook = z.object({
+export const GenerateStoryContent = z.object({
   childName: z.string(),
+  childAge: z.number(),
+  childGender: z.enum(["male", "female"]),
+  artStyle: z.enum(["soft-anime", "pixar", "watercolor", "block-art"]),
+  values: z.array(z.string()),
+  theme: z.string(),
+  customTheme: z.string().optional().nullable(),
 });
+
+export const GenerateStoryImage = z.object({
+  prompt: z.string(),
+  photos: z.array(z.string()),
+});
+
+export type GenerateStoryContent = z.infer<typeof GenerateStoryContent>;
+export type GenerateStoryImage = z.infer<typeof GenerateStoryImage>;
